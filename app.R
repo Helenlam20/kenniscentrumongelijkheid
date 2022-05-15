11957d88e389b609a82fa51139eab674814cef53
koenlam <47334582+koenlam@users.noreply.github.com>
2022-05-15 15:27:01 +0200

    Made the shiny app deployable
     - Add rsconnect folder to .gitignore
     - Moved startup code from app.R to startup.R
     - Added the missing markdown library to app.R
     - Added a deploy.R script to deloy the app

--- app.R
+++ app.R
@@ -21,34 +21,12 @@ library(plotly)
 library(readxl)
 library(shinyBS)
 library(grid)
-
-
-#### LOAD DATA ####
-outcome_dat <- read_excel("./data/outcome_table.xlsx", sheet = "outcome")
-area_dat <- read_excel("./data/outcome_table.xlsx", sheet = "area")
-
-
-for (i in c("bins_20", "bins_10", "bins_5", "total", "parents_edu")) {
-  
-  assign(i, read_rds(file.path("./data/", paste0(i, "_tab.rds"))))
-  
-}
-rm(i)
-
-# combines gradient data
-gradient_dat <- bind_rows(bins_20, bins_10) 
-gradient_dat <- bind_rows(gradient_dat, bins_5)
-gradient_dat <- bind_rows(gradient_dat, total)
-gradient_dat <- bind_rows(gradient_dat, parents_edu) 
-rm(bins_20, bins_10, bins_5, total, parents_edu)
-
-
-# txt file for README in download button for data and fig
-temp_txt <- paste(readLines("./data/README.txt"))
+library(markdown)
 
 
 
 #### RUN APP ####
+source("./startup.R")
 source("./code/Theme_PoorMansFlatly.R")
 source("./code/ui.R")
 source("./code/server.R")
