cf0e0257073914de558fd85bbdd7cf93cfbe8ef2
koenlam <47334582+koenlam@users.noreply.github.com>
2022-05-18 12:11:08 +0200

    Added custom css

--- www/script.js
+++ www/script.js
@@ -6,8 +6,3 @@ var box_btn = document.getElementById('box_groene_group').closest('.box').getEle
 box_btn.style.visibility='hidden';
 // Add an listener to the OnePlot toggle to also click the hidden collapse button
 document.getElementById('OnePlot').addEventListener('change', function(){box_btn.click()})
-
-
-
-// Hide barplot button
-document.getElementById("change_barplot").style.visibility='hidden';
