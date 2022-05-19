332077658e28a1abd2e2a758734e8e5d62ceac34
koenlam <47334582+koenlam@users.noreply.github.com>
2022-05-19 18:01:06 +0200

    Added an icon to the titlebar

--- www/script.js
+++ www/script.js
@@ -7,4 +7,8 @@ box_btn.style.visibility='hidden';
 // Add an listener to the OnePlot toggle to also click the hidden collapse button
 document.getElementById('OnePlot').addEventListener('change', function(){box_btn.click()});
 
+// Hide the alterative plot button
 document.getElementById('change_barplot').closest('div').style.display='none';
+
+// Add an icon to the title bar
+$('head').append('<link rel = "icon", type = "image/png", href = "temp_home.png">');
