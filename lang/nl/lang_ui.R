33e84ba59e8e372fc58fdfc3ee940d65b683fb66
koenlam <47334582+koenlam@users.noreply.github.com>
2023-03-03 20:51:20 +0100

    Fixed newline in license

--- lang/nl/lang_ui.R
+++ lang/nl/lang_ui.R
@@ -198,8 +198,4 @@ lang[["label_neighbourhood"]] <- "Wijk"
 
 
 # License
-lang[["license_text"]] <- "Deze figuur is gemaakt door Helen Lam, Bastian Ravesteijn en Coen van de Kraats 
-(Erasmus School of Economics), met ondersteuning van Kenniscentrum Ongelijkheid. De figuur en onderliggende data 
-zijn beschikbaar volgens een Creative Commons BY-NC-SA 4.0 licentie, altijd 
-onder vermelding van auteurs en de website ongelijkheidincijfers.amsterdam. 
-Bij vragen kunt u contact opnemen met ravesteijn@ese.eur.nl"
+lang[["license_text"]] <- "Deze figuur is gemaakt door Helen Lam, Bastian Ravesteijn en Coen van de Kraats (Erasmus School of Economics), met ondersteuning van Kenniscentrum Ongelijkheid. De figuur en onderliggende data zijn beschikbaar volgens een Creative Commons BY-NC-SA 4.0 licentie, altijd onder vermelding van auteurs en de website ongelijkheidincijfers.amsterdam. Bij vragen kunt u contact opnemen met ravesteijn@ese.eur.nl"
