d34e78424cb5436605fb327bc74c47a2cbddc621
sjileaeva <jileaeva@ese.eur.nl>
2025-02-20 16:24:28 +0100

    new data (#104)
    * bins 10
    
    * bins 20
    
    * bins5 p1
    
    * bins5 p2
    
    * mean p1
    
    * mean p2
    
    * parents edu
    
    * edit code to read new data files
    
    ---------
    
    Co-authored-by: Helen Lam <49119473+Helenlam20@users.noreply.github.com>

--- startup.R
+++ startup.R
@@ -5,19 +5,28 @@ outcome_dat <- read_excel(lang[["loc_outome_table.xlsx"]], sheet = "outcome")
 area_dat <- read_excel(lang[["loc_outome_table.xlsx"]], sheet = "area")
 
 
-for (i in c("bins20", "bins10", "bins5", "mean", "parents_edu")) {
-  
-  assign(i, read_rds(file.path(lang[["loc_data_rds"]] , paste0(i, "_tab.rds"))))
-  
-}
-rm(i)
+csv_files <- list.files(path = lang[["loc_data_rds"]], pattern = "\\.csv$", full.names = TRUE)
 
-# combines gradient data
-gradient_dat <- bind_rows(bins20, bins10) 
-gradient_dat <- bind_rows(gradient_dat, bins5)
-gradient_dat <- bind_rows(gradient_dat, mean)
-gradient_dat <- bind_rows(gradient_dat, parents_edu) 
-rm(bins20, bins10, bins5, mean, parents_edu)
+
+read_and_cast_csv <- function(file) {
+  dt <- fread(file)
+  
+  dt[, N := as.numeric(N)]
+  dt[, parents_income := as.numeric(parents_income)]
+  dt[, mean := as.numeric(mean)]
+  
+  # Ensure all other columns are of type character
+  dt <- dt %>% mutate(across(-c(N, mean, parents_income), as.character))
+  
+  
+  return(dt)
+}
+
+cols_fact <- c("geografie", "geslacht", "migratieachtergrond", "huishouden", "bins", "opleiding_ouders", "uitkomst",
+               "type")
+
+gradient_dat <- csv_files %>% map_dfr(~read_and_cast_csv(.x)) %>%
+  mutate_at(cols_fact, factor) 
 
 
 # txt file for README in download button for data and fig
